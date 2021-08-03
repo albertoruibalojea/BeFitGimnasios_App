6d4aba3318220dca85cf8a83c85a22fb5357ea98
albertoruibalojea <alberto.ruibal.ojea@outlook.es>
2021-08-03 21:17:37 +0200

    Add files via upload

---
+++ SQL/creacionTablas.sql
@@ -0,0 +1,216 @@
+﻿Create sequence seq_equipamiento_id_equipamiento;
+Create sequence seq_actividad_id_actividad;
+Create sequence seq_sesion_id_sesion;
+
+
+Create table equipamiento (
+  modelo varchar(60) not null ,
+  id_equipamiento integer default nextval('seq_equipamiento_id_equipamiento') not null primary key
+);
+
+Create table actividad (
+  nombre varchar(60) not null,
+  id_actividad integer default nextval('seq_actividad_id_actividad') not null primary key
+);
+
+Create table establecimiento(
+  id_establecimiento integer not null primary key,
+  nombre varchar(60),
+  ciudad varchar(60),
+  telefono varchar(15),
+  direccion varchar(100) NOT NULL UNIQUE
+);
+
+Create table monitor(
+  dni_monitor varchar(9) not null primary key,
+  nombre varchar(60),
+  telefono varchar(15),
+  email varchar(60),
+  horario varchar(7),
+  sueldo integer,
+  ano_contratacion integer not null,
+
+  establecimiento integer not null,
+  foreign key (establecimiento) references establecimiento(id_establecimiento)
+    on delete cascade on update cascade
+);
+
+Create table sesion(
+  actividad integer default currval ('seq_actividad_id_actividad') not null,
+  id_sesion integer default nextval('seq_sesion_id_sesion') not null,
+  dni_monitor varchar (9) not null,
+  fecha_inicio varchar(30) not null ,
+  plazas integer not null,
+  establecimiento integer not null,
+
+  primary key (id_sesion, establecimiento),
+  foreign key (establecimiento) references establecimiento(id_establecimiento)
+    on delete cascade on update cascade,
+  foreign key(actividad) references actividad(id_actividad)
+    on delete cascade on update cascade,
+  foreign key (dni_monitor) references monitor (dni_monitor)
+    on delete cascade on update cascade
+);
+
+
+--Secuencias
+create or replace function controla_secuencias_equipamiento() returns trigger as $cse$
+  declare
+    ejemplar integer;
+  begin
+    case TG_OP
+      when 'INSERT' then
+        execute 'create sequence seq_ejemplares_equipamiento_' ||cast(NEW.id_equipamiento as text);
+        return new;
+      when 'UPDATE' then
+        IF OLD.id_equipamiento <> NEW.id_equipamiento THEN
+          ejemplar := nextval ('seq_ejemplares_equipamiento_'||cast(OLD.id_equipamiento as text));
+          execute 'drop sequence seq_ejemplares_equipamiento_'||cast(OLD.id_equipamiento as text);
+          execute 'create sequence seq_ejemplares_equipamiento_'||cast(NEW.id_equipamiento as text)|| ' start '||cast(ejemplar as text);
+        END IF;
+        return new;
+      when 'DELETE' then
+        execute 'drop sequence seq_ejemplares_equipamiento_'||cast(OLD.id_equipamiento as text);
+        return old;
+      else null;
+    end case;
+  end;
+  $cse$ Language plpgsql;
+
+create or replace function controla_secuencias_actividad() returns trigger as $csa$
+  declare
+    actividad integer;
+  begin
+    case TG_OP
+      when 'INSERT' then
+        execute 'create sequence seq_sesiones_actividad_' ||cast(NEW.id_actividad as text);
+        return new;
+      when 'UPDATE' then
+        IF OLD.id_actividad <> NEW.id_actividad THEN
+          actividad := nextval ('seq_sesiones_actividad_'||cast(OLD.id_actividad as text));
+          execute 'drop sequence seq_sesiones_actividad_'||cast(OLD.id_actividad as text);
+          execute 'create sequence seq_sesiones_actividad_'||cast(NEW.id_actividad as text)|| ' start '||cast(sesion as text);
+        END IF;
+        return new;
+      when 'DELETE' then
+        execute 'drop sequence seq_sesiones_actividad_'||cast(OLD.id_actividad as text);
+        return old;
+      else null;
+    end case;
+  end;
+  $csa$ Language plpgsql;
+
+create or replace function controla_secuencias_sesion() returns trigger as $css$
+  declare
+    sesion integer;
+  begin
+    case TG_OP
+      when 'INSERT' then
+        execute 'create sequence seq_sesion_id_sesion' ||cast(NEW.id_sesion as text);
+        return new;
+      when 'UPDATE' then
+        IF OLD.id_sesion <> NEW.id_sesion THEN
+          sesion := nextval ('seq_sesion_id_sesion'||cast(OLD.id_sesion as text));
+          execute 'drop sequence seq_sesion_id_sesion'||cast(OLD.id_sesion as text);
+          execute 'create sequence seq_sesion_id_sesion'||cast(NEW.id_sesion as text)|| ' start '||cast(sesion as text);
+        END IF;
+        return new;
+      when 'DELETE' then
+        execute 'drop sequence seq_sesion_id_sesion'||cast(OLD.id_sesion as text);
+        return old;
+      else null;
+    end case;
+  end;
+  $css$ Language plpgsql;
+
+
+--Disparadores
+create trigger afterEquipamientos after insert or update or delete on equipamiento
+  for each row execute procedure controla_secuencias_equipamiento();
+
+create trigger afterActividades after insert or update or delete on actividad
+  for each row execute procedure controla_secuencias_actividad();
+
+create trigger afterSesiones after insert or update or delete on sesion
+  for each row execute procedure controla_secuencias_sesion();
+
+
+create or replace function introduce_id_actividad() returns trigger as $csa$
+  begin
+    new.id_actividad:=nextval('seq_sesiones_actividad_'||cast(new.actividad as text));
+  return new;
+end;
+$csa$ Language plpgsql;
+
+create or replace function introduce_id_sesion() returns trigger as $css$
+  begin
+    new.id_sesion:=nextval('seq_sesion_id_sesion'||cast(new.sesion as text));
+  return new;
+end;
+$css$ Language plpgsql;
+
+
+
+create table ejemplar(
+  equipamiento integer default currval ('seq_equipamiento_id_equipamiento') not null,
+  id_ejemplar integer not null,
+  descripcion varchar(100),
+  ano_compra integer not null,
+  establecimiento integer not null,
+
+  primary key (equipamiento, id_ejemplar),
+  foreign key (equipamiento) references equipamiento(id_equipamiento)
+      on delete cascade on update cascade,
+  foreign key (establecimiento) references establecimiento (id_establecimiento)
+	on delete cascade on update cascade
+);
+
+--Disparador de ejemplar
+create or replace function introduce_id_ejemplar() returns trigger as $cse$
+  begin
+    new.id_ejemplar:=nextval('seq_ejemplares_equipamiento_'||cast(new.equipamiento as text));
+    return new;
+  end;
+  $cse$ Language plpgsql;
+
+create trigger beforeInsertEjemplares before insert on ejemplar
+ for each row execute procedure introduce_id_ejemplar();
+
+
+
+
+
+Create table usuario (
+  dni_usuario varchar (9) not null primary key,
+  nombre varchar(60),
+  email varchar (60),
+  telefono varchar(15),
+  socio boolean not null
+);
+
+Create table administrador(
+  dni_admin varchar(9) not null primary key,
+  contrasena varchar(16) not null,
+  nombre varchar(60),
+  telefono varchar(15),
+  email varchar(60),
+  sueldo integer,
+  ano_contratacion integer not null,
+
+  establecimiento integer not null,
+  foreign key (establecimiento) references establecimiento(id_establecimiento)
+    on delete cascade on update cascade
+);
+
+Create table usuario_en_sesion
+(
+  usuario varchar (9) not null,
+  sesion integer default currval('seq_sesion_id_sesion') not null,
+  establecimiento integer not null,
+
+  primary key (usuario,sesion,establecimiento),
+  foreign key (usuario) references usuario(dni_usuario)
+    on delete restrict on update cascade,
+  foreign key (sesion,establecimiento) references sesion(id_sesion,establecimiento)
+    on delete cascade on update cascade
+);
---
+++ SQL/datos.sql
@@ -0,0 +1,70 @@
+﻿Insert into usuario values ('54127855T', 'Sofia Martinez',   'sofia.marpa25@gmail.com', 698149747, true);
+Insert into usuario values ('43678572G', 'Juan Perez',       'jp@gmail.com',            634848275, true);
+Insert into usuario values ('93475284M', 'Javier Alvarez',   'javialv@gmail.com',       628492748, false);
+Insert into usuario values ('41998572G', 'Laura Arias',      'larias@gmail.com',        601938472, false);
+Insert into usuario values ('43926572F', 'Lara Padrón',      'larapadron@outlook.es',   656743224, false);
+Insert into usuario values ('43975262K', 'María del Carmen', 'mcarmen@gmail.com',       651671472, false);
+
+
+Insert into establecimiento values (0, 'Seiken',      'A Coruña', 981234567, 'c/Orzan, 18');
+Insert into establecimiento values (1, 'Olympo',      'Santiago', 981485649, 'c/Romero Donallo, 25');
+Insert into establecimiento values (2, 'Fit Factory', 'Vigo',     986454649, 'c/Val Miñor, 70');
+
+
+Insert into equipamiento (modelo) values ('Pesas');
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 0);
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 0);
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 1);
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 2);
+Insert into equipamiento (modelo) values ('Maquina de Correr');
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 0);
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 1);
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 1);
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 2);
+Insert into equipamiento (modelo) values ('Esterilla');
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 0);
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 1);
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 2);
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 2);
+Insert into equipamiento (modelo) values ('Bicicleta Elíptica');
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 0);
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 1);
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 2);
+Insert into equipamiento (modelo) values ('Stepper');
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 0);
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 1);
+Insert into ejemplar (ano_compra, establecimiento) values (2018, 2);
+
+Insert into administrador (dni_admin, contrasena, nombre, telefono, email, ano_contratacion, establecimiento) values ('58274953F', 'pwdmarta',  'marta',  685629457, 'marta@befit.com',  2018, 0);
+Insert into administrador (dni_admin, contrasena, nombre, telefono, email, ano_contratacion, establecimiento) values ('58234533P', 'pwdadrian', 'Adrian', 682346546, 'adrian@befit.com', 2018, 1);
+Insert into administrador (dni_admin, contrasena, nombre, telefono, email, ano_contratacion, establecimiento) values ('58234458P', 'pwdlaura',  'Laura',  687678846, 'laura@befit.com',  2018, 2);
+
+
+Insert into monitor (dni_monitor, nombre, telefono, email, horario, ano_contratacion, establecimiento) values ('38183753R', 'Martin',   683653958, 'martin@mbefit.com',     'mañana', 2018, 0);
+Insert into monitor (dni_monitor, nombre, telefono, email, horario, ano_contratacion, establecimiento) values ('54563453E', 'Manuel',   683654567, 'manuel@mbefit.com',     'mañana', 2018, 0);
+Insert into monitor (dni_monitor, nombre, telefono, email, horario, ano_contratacion, establecimiento) values ('44343427J', 'Pedro',    664593222, 'pedro@mbefit.com',      'tarde',  2019, 0);
+Insert into monitor (dni_monitor, nombre, telefono, email, horario, ano_contratacion, establecimiento) values ('11233347U', 'Santiago', 612345678, 'santiago@mbefit.com',   'tarde',  2019, 0);
+Insert into monitor (dni_monitor, nombre, telefono, email, horario, ano_contratacion, establecimiento) values ('21385347G', 'Alberto',  671293857, 'alberto@mbefit.com',    'tarde',  2019, 1);
+Insert into monitor (dni_monitor, nombre, telefono, email, horario, ano_contratacion, establecimiento) values ('75395146L', 'María',    951245369, 'maría@mbefit.com',      'tarde',  2019, 1);
+Insert into monitor (dni_monitor, nombre, telefono, email, horario, ano_contratacion, establecimiento) values ('35124784N', 'Mónica',   624513798, 'monica@mbefit.com',     'mañana', 2019, 1);
+Insert into monitor (dni_monitor, nombre, telefono, email, horario, ano_contratacion, establecimiento) values ('45214789P', 'Lucía',    645127389, 'lucia@mbefit.com',      'mañana', 2019, 1);
+Insert into monitor (dni_monitor, nombre, telefono, email, horario, ano_contratacion, establecimiento) values ('21387847G', 'Raul',     692847268, 'raul@mbefit.com',       'tarde',  2019, 2);
+Insert into monitor (dni_monitor, nombre, telefono, email, horario, ano_contratacion, establecimiento) values ('47851269R', 'Paula',    685124539, 'paula@mbefit.com',      'tarde',  2019, 2);
+Insert into monitor (dni_monitor, nombre, telefono, email, horario, ano_contratacion, establecimiento) values ('24157937S', 'Rodrigo',  678451379, 'rodrigo@mbefit.com',    'mañana', 2019, 2);
+Insert into monitor (dni_monitor, nombre, telefono, email, horario, ano_contratacion, establecimiento) values ('58741298W', 'Atenea',   631245798, 'atenea@mbefit.com',     'mañana', 2019, 2);
+
+
+
+Insert into actividad values ('Zumba',   0);
+Insert into actividad values ('Spinning',1);
+Insert into actividad values ('Boxeo',   2);
+Insert into actividad values ('Danza',   3);
+Insert into actividad values ('Acroyoga',4);
+
+Insert into sesion (actividad, dni_monitor,fecha_inicio,plazas,establecimiento) values(0, '38183753R',  '29/04/2019 10:00:00', 20, 0);
+Insert into sesion (actividad, dni_monitor,fecha_inicio,plazas,establecimiento) values(1, '21385347G',  '29/04/2019 17:00:00', 20, 1);
+Insert into sesion (actividad, dni_monitor,fecha_inicio,plazas,establecimiento) values(2, '21387847G',  '29/04/2019 15:00:00', 20, 2);
+
+INSERT INTO usuario_en_sesion(usuario, sesion, establecimiento) VALUES ('93475284M', 1, 0);
+INSERT INTO usuario_en_sesion(usuario, sesion, establecimiento) VALUES ('54127855T', 2, 1);
+INSERT INTO usuario_en_sesion(usuario, sesion, establecimiento) VALUES ('43975262K', 3, 2);
